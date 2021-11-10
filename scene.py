3214ce998f77f4c0a772475795cae7bb08147070
Ignacio Baptista [PC] <ibaptista@frba.utn.edu.ar>
2021-11-10 16:04:30 -0300

    Agregadas escenas varias

--- scene.py
+++ scene.py
@@ -87,50 +87,30 @@ class DiferentesSenos(Scene):
 
 class FuncionDienteDeSierra(Scene):
     def construct(self):
-        # Ejes
-        #ax = Axes(x_range=[-6, 6, 1], y_range=[-5, 5, 1], x_length=12, y_length=6).add_coordinates()
-        # Lineas del plano
-        plane = NumberPlane(x_range=[-6, 6, 1], y_range=[-5, 5, 1],
-                            x_length=10, y_length=8,
-                            background_line_style={"stroke_color": TEAL,
-                                                   "stroke_width": 2,
-                                                   "stroke_opacity": 0.2}).add_coordinates()
-
-        plane.center()
-        #ax.center()
-        line_graph = plane.plot_line_graph(
-            x_values=[0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5],
-            y_values=[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-            line_color=GOLD_E,
-            vertex_dot_style=dict(stroke_width=3, fill_color=PURPLE),
-            stroke_width=4, add_vertex_dots=False
-        )
-
-        valorMedio = plane.plot(lambda x: 0.5, color=DARK_BLUE)
-
-        self.play(Create(plane, run_time=2))
-
-        self.play(Create(line_graph, run_time=2))
+        plane = self.mostrarPlano()
 
         self.wait(1)
 
-        self.play(Create(valorMedio, run_time=2))
+        formula = Tex(r"f(x) = x si x $\epsilon$ (0,1) $\linebreak$"
+                      r"f(x) = f(x+1)", font_size=42)
+        formula.shift(UP * 3, LEFT * 3)
+        self.play(Create(formula), run_time=2)
 
-        animations = []
+        self.wait(1)
 
-        for i in range(0, 5):
-            funcionCreciente = plane.plot(lambda x: x - i, color=DARK_BLUE)
-            area = plane.get_area(funcionCreciente, bounded_graph=valorMedio,
-                               x_range=(i, i + 1),
-                               opacity=0.4)
-            animations.append(Create(area, run_time=2))
+        self.mostrarLineGraph(plane)
 
-        self.play(animations[0], animations[1], animations[2], animations[3], animations[4])
+        self.wait(1)
+
+        valorMedio = self.mostrarValorMedio(plane)
+
+        self.wait(1)
+
+        self.mostrarArea(plane, valorMedio)
 
         self.wait(3)
 
-class FuncionCuadratica(Scene):
-    def construct(self):
+    def mostrarPlano(self):
         # Lineas del plano
         plane = NumberPlane(x_range=[-6, 6, 1], y_range=[-6, 6, 1],
                             x_length=7, y_length=7,
@@ -139,73 +119,131 @@ class FuncionCuadratica(Scene):
                                                    "stroke_opacity": 0.2}).add_coordinates().center()
 
         self.play(Create(plane, run_time=2))
+        return plane
+
+    def mostrarLineGraph(self, plane):
+        line_graph = plane.plot_line_graph(
+            x_values=[0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6],
+            y_values=[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            line_color=GOLD_E,
+            vertex_dot_style=dict(stroke_width=3, fill_color=PURPLE),
+            stroke_width=4, add_vertex_dots=False
+        )
+
+        self.play(Create(line_graph, run_time=2))
+
+    def mostrarValorMedio(self, plane):
+        valorMedio = plane.plot(lambda x: 0.5, color=DARK_BLUE)
+        self.play(Create(valorMedio, run_time=2))
+        return valorMedio
+
+    def mostrarArea(self, plane, valorMedio):
+        animations = []
+
+        for i in range(0, 6):
+            funcionCreciente = plane.plot(lambda x: x - i, color=DARK_BLUE)
+            area = plane.get_area(funcionCreciente, bounded_graph=valorMedio, x_range=(i, i + 1), opacity=0.4)
+            animations.append(Create(area, run_time=2))
+
+        self.play(animations[0], animations[1], animations[2], animations[3], animations[4])
+
+
+class FuncionCuadratica(Scene):
+    xMin = -6
+    xMax = 6
+
+    def construct(self):
+
+        plano2D = self.mostrarPlano()
 
         self.wait(1)
 
         formula = Tex(r"f(x) = x$^2$ si x $\epsilon$ (0,1) $\linebreak$"
                       r"f(x) = f(x+1)", font_size=42)
-        formula.shift(UP * 3)
-        formula.shift(LEFT * 3)
-
-        self.play(Create(formula))
+        formula.shift(UP * 3, LEFT * 3)
+        self.play(Create(formula), run_time=2)
 
         self.wait(2)
 
+        self.mostrarEquisCuadrado(plano2D)
+
+        self.wait(2)
+
+        valorMedio = self.mostrarValorMedio(plano2D)
+
+        self.wait(2)
+
+        self.mostrarArea(plano2D, valorMedio)
+
+        self.wait(3)
+
+    def mostrarEquisCuadrado(self, plane):
         animations = []
 
-        for i in range(0, 5):
-            equisCuadrado = plane.plot(lambda x: (x-i)*(x-i), color=DARK_BLUE, x_range=(i, i+1))
+        for i in range(self.xMin, self.xMax):
+            equisCuadrado = plane.plot(lambda x: (x - i) * (x - i), color=DARK_BLUE, x_range=(i, i + 1))
             animations.append(Create(equisCuadrado, run_time=2))
 
-        valorMedio = plane.plot(lambda x: 0.5, color=ORANGE)
-
-        self.play(animations[0])
+        self.play(animations[6])
         self.wait(1)
-        self.play(animations[1], animations[2], animations[3], animations[4])
+        self.play(animations[0], animations[1], animations[2], animations[3], animations[4], animations[5])
+        self.wait(0.5)
+        self.play(animations[7], animations[8], animations[9], animations[10], animations[11])
 
-        self.wait(1)
-
+    def mostrarValorMedio(self, plane):
+        valorMedio = plane.plot(lambda x: 0.3, color=ORANGE)
         self.play(Create(valorMedio, run_time=2))
+        return valorMedio
 
-        self.wait(1)
-
+    def mostrarArea(self, plane, valorMedio):
         animations = []
 
-        for i in range(0, 5):
+        for i in range(self.xMin, self.xMax):
             equisCuadrado = plane.plot(lambda x: (x - i) * (x - i), color=DARK_BLUE, x_range=(i, i + 1))
             area = plane.get_area(valorMedio, bounded_graph=equisCuadrado,
                                   x_range=(i, i + 1),
                                   opacity=0.4)
             animations.append(Create(area, run_time=3))
 
-        self.play(animations[0], animations[1], animations[2], animations[3], animations[4])
+        self.play(animations[0], animations[1], animations[2], animations[3], animations[4], animations[5])
 
-        self.wait(3)
+    def mostrarPlano(self):
+        # Lineas del plano
+        plane = NumberPlane(x_range=[-6, 6, 1], y_range=[-6, 6, 1],
+                            x_length=7, y_length=7,
+                            background_line_style={"stroke_color": TEAL,
+                                                   "stroke_width": 2,
+                                                   "stroke_opacity": 0.2}).add_coordinates().center()
 
+        self.play(Create(plane, run_time=2))
+        return plane
 
 
 class EscalonUnitario(Scene):
     def construct(self):
-        # Ejes
-        ax = Axes(x_range=[-6, 6, 1], y_range=[-3, 3, 1], x_length=12, y_length=6).add_coordinates()
-        # Lineas del plano
-        plane = NumberPlane(x_range=[-6, 6, 1], y_range=[-3, 3, 1],
-                            background_line_style={"stroke_color": TEAL,
-                                                   "stroke_width": 2,
-                                                   "stroke_opacity": 0.2})
+        plane = self.mostrarPlano()
 
-        plane.center()
+        # noinspection PyTypeChecker
         line_graph = plane.plot_line_graph(
-            x_values=[0, 5],
+            x_values=[0, 6],
             y_values=[1, 1],
             line_color=GOLD_E,
             vertex_dot_style=dict(stroke_width=3, fill_color=PURPLE),
-            stroke_width=4, add_vertex_dots=False
-        )
+            stroke_width=4, add_vertex_dots=False)
 
-        self.play(Create(ax, run_time=2),
-                  Create(plane, run_time=2))
+        self.wait(1)
 
         self.play(Create(line_graph, run_time=2))
 
         self.wait(3)
+
+    def mostrarPlano(self):
+        # Lineas del plano
+        plane = NumberPlane(x_range=[-6, 6, 1], y_range=[-6, 6, 1],
+                            x_length=7, y_length=7,
+                            background_line_style={"stroke_color": TEAL,
+                                                   "stroke_width": 2,
+                                                   "stroke_opacity": 0.2}).add_coordinates().center()
+
+        self.play(Create(plane, run_time=2))
+        return plane
